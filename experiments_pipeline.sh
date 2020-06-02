f6a6f25a160d446ef7757b388081106665b201de
Maike Guderlei <maike@guderlei.de>
2020-06-02 19:51:31 +0200

    example script to run several experiments in one session

--- experiments_pipeline.sh
+++ experiments_pipeline.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
-#time python3 experiments.py --model="bert" --model_type="bert-base-cased" --freeze="freeze" --dataset_name="fnc_arc" --seed=3
-
-#time python3 experiments.py --model="bert" --model_type="bert-base-cased" --freeze="freeze" --dataset_name="fnc_arc" --seed=5
-
-#time python3 experiments.py --model="bert" --model_type="bert-base-cased" --freeze="freeze" --dataset_name="fnc_arc" --seed=7
+# bert freeze fnc arc
+time_var=$(date) 
+python3 experiments.py --model="bert" --model_type="bert-base-cased" --freeze="freeze" --dataset_name="fnc_arc" --seed=3 2>&1 | tee -a bert_experiment_freeze_seed_3_fnc_arc.log 
+echo $time_var 2>&1 | tee -a bert_experiment_freeze_seed_3_fnc_arc.log 
+date 2>&1 | tee -a bert_experiment_freeze_seed_3_fnc_arc.log 
 
 time_var=$(date) 
-#ps -ef &>> test.log
-python3 experiments.py --model="distilbert" --model_type="distilroberta-base" --freeze="freeze" --dataset_name="fnc" --seed=3 2>&1 | tee -a distilbert_experiment_freeze_seed_3_fnc.log 
-echo $time_var 2>&1 | tee -a distilbert_experiment_freeze_seed_3_fnc.log
-date 2>&1 | tee -a distilbert_experiment_freeze_seed_3_fnc.log
+python3 experiments.py --model="bert" --model_type="bert-base-cased" --freeze="freeze" --dataset_name="fnc_arc" --seed=5 2>&1 | tee -a bert_experiment_freeze_seed_5_fnc_arc.log 
+echo $time_var 2>&1 | tee -a bert_experiment_freeze_seed_5_fnc_arc.log 
+date 2>&1 | tee -a bert_experiment_freeze_seed_5_fnc_arc.log 
 
 time_var=$(date) 
-#ps -ef &>> test.log
-python3 experiments.py --model="distilbert" --model_type="distilroberta-base" --freeze="freeze" --dataset_name="fnc" --seed=5 2>&1 | tee -a distilbert_experiment_freeze_seed_5_fnc.log 2>&1
-echo $time_var 2>&1 | tee -a distilbert_experiment_freeze_seed_5_fnc.log
-date 2>&1 | tee -a distilbert_experiment_freeze_seed_5_fnc.log
+python3 experiments.py --model="bert" --model_type="bert-base-cased" --freeze="freeze" --dataset_name="fnc_arc" --seed=7 2>&1 | tee -a bert_experiment_freeze_seed_7_fnc_arc.log 
+echo $time_var 2>&1 | tee -a bert_experiment_freeze_seed_7_fnc_arc.log 
+date 2>&1 | tee -a bert_experiment_freeze_seed_7_fnc_arc.log 
+
+
+# roberta freeze fnc arc
+time_var=$(date) 
+python3 experiments.py --model="roberta" --model_type="roberta-base" --freeze="freeze" --dataset_name="fnc_arc" --seed=3 2>&1 | tee -a roberta_experiment_freeze_seed_3_fnc_arc.log 
+echo $time_var 2>&1 | tee -a roberta_experiment_freeze_seed_3_fnc_arc.log 
+date 2>&1 | tee -a roberta_experiment_freeze_seed_3_fnc_arc.log 
+
+time_var=$(date) 
+python3 experiments.py --model="roberta" --model_type="roberta-base" --freeze="freeze" --dataset_name="fnc_arc" --seed=5 2>&1 | tee -a roberta_experiment_freeze_seed_5_fnc_arc.log 
+echo $time_var 2>&1 | tee -a roberta_experiment_freeze_seed_5_fnc_arc.log 
+date 2>&1 | tee -a roberta_experiment_freeze_seed_5_fnc_arc.log 
+
+time_var=$(date) 
+python3 experiments.py --model="roberta" --model_type="roberta-base" --freeze="freeze" --dataset_name="fnc_arc" --seed=7 2>&1 | tee -a roberta_experiment_freeze_seed_7_fnc_arc.log 
+echo $time_var 2>&1 | tee -a roberta_experiment_freeze_seed_7_fnc_arc.log 
+date 2>&1 | tee -a roberta_experiment_freeze_seed_7_fnc_arc.log 
